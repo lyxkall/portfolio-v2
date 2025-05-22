1c9b3b08d55c834d6ce9fea2ca8c1cd0a47f6dc8
lyxkall <tatangmc3@gmail.com>
2025-05-22 18:45:20 +0700

    First Commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://lyxkall.my.id', // Ganti ke domain lo
+  generateRobotsTxt: true, // Wajib true biar robots.txt ke-generate
+  sitemapSize: 7000, // Default aman
+}
